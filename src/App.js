c3452d42f2cbddcc7aa93542c24955f59cb717c2
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2020-11-14 19:25:45 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <label htmlFor="nome">Nome</label>
+      <input id="nome" type="text" />
+    </>
+  );
+}
+
+export default App;
