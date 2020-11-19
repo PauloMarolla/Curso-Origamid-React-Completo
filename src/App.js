9e731fcb10ef413aa3c555fec5577138945c3f15
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2020-11-19 19:54:19 -0300

    React forms input 1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React from 'react';
-import useFetch from './useFetch';
 
 const App = () => {
-  const { request, data, loading, error } = useFetch();
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+  });
 
-  React.useEffect(() => {
-    async function fetchData() {
-      const { response, json } = await request(
-        'https://ranekapi.origamid.dev/json/api/produto/',
-      );
-      console.log(response);
-    }
-    fetchData();
-  }, [request]);
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
-  console.log(data);
+  // funcao que deixa todos os campos do form como reativos
+  function handleChange({ target }) {
+    const { id, value } = target;
+    // [id] ta entre colchete, justamente para ele acessar a propriedade de cada ID, e n passar o digito 'id'
+    setForm({ ...form, [id]: value });
+  }
 
-  if (error) return <p>{error}</p>;
-  if (loading) return <p>Carregando...</p>;
-  if (data)
-    return (
-      <>
-        <ul>
-          {data.map((produto) => {
-            return <li key={produto.id}>{produto.nome}</li>;
-          })}
-        </ul>
-      </>
-    );
-  else return null;
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nome">Nome</label>
+        <input
+          type="text"
+          id="nome"
+          value={form.nome}
+          onChange={handleChange}
+        />
+        {form.nome}
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          value={form.email}
+          onChange={handleChange}
+        />
+        {form.email}
+        <button>Enviar</button>
+      </form>
+    </>
+  );
 };
 
 export default App;
