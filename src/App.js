388b25fa9d857dd1b60d1226934953638a75da65
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2020-11-20 20:15:40 -0300

    React forms select

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 // nome, email, senha, cep, rua, numero, bairro, cidade e estado
 const App = () => {
-  const [textarea, setTextarea] = React.useState('');
+  const [select, setselect] = React.useState('');
 
   return (
     <>
       <form action="">
-        <textarea
+        <select
           id=""
-          value={textarea}
-          onChange={({ target }) => setTextarea(target.value)}
-        ></textarea>
-        {textarea}
+          value={select}
+          onChange={({ target }) => setselect(target.value)}
+        >
+          <option value="" disabled>
+            Selecione
+          </option>
+          <option value="notebook">Notebook</option>
+          <option value="smartphone">Smartphone</option>
+          <option value="tablet">Tablet</option>
+        </select>
+        {select}
       </form>
     </>
   );
