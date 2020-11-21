5cbe11bb63d5a8edd333a5a5046468c986ccd723
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2020-11-21 13:40:19 -0300

    React forms input radio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-// nome, email, senha, cep, rua, numero, bairro, cidade e estado
+
 const App = () => {
-  const [select, setselect] = React.useState('');
+  const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
+
+  function handleChange({ target }) {
+    setProduto(target.value);
+  }
 
   return (
     <>
+      {/* Essa é a primeira forma de fazer o input radio ser reativo, passando o name unico */}
+      <h2>Produto</h2>
+      {produto}
       <form action="">
-        <select
-          id=""
-          value={select}
-          onChange={({ target }) => setselect(target.value)}
-        >
-          <option value="" disabled>
-            Selecione
-          </option>
-          <option value="notebook">Notebook</option>
-          <option value="smartphone">Smartphone</option>
-          <option value="tablet">Tablet</option>
-        </select>
-        {select}
+        <label>
+          <input
+            onChange={handleChange}
+            name="produto"
+            value="smartphone"
+            type="radio"
+          />
+          Smartphone
+        </label>
+        <label>
+          <input
+            onChange={handleChange}
+            name="produto"
+            value="notebook"
+            type="radio"
+          />
+          Notebook
+        </label>
+        <h2>Cor</h2>
+        {/* essa é a segunda forma de fazer o input radio reativo, passando o checked da variavel comparando com o valor */}
+        {cor}
+        <label>
+          <input
+            onChange={({ target }) => setCor(target.value)}
+            value="azul"
+            checked={cor === 'azul'}
+            type="radio"
+          />
+          Azul
+        </label>
+        <label>
+          <input
+            onChange={({ target }) => setCor(target.value)}
+            value="vermelho"
+            type="radio"
+            checked={cor === 'vermelho'}
+          />
+          Vermelho
+        </label>
       </form>
     </>
   );
