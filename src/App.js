78f937e829a35ba8d248bcfc070263d4daa1ee9d
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2020-11-21 15:15:38 -0300

    Form input Radio componente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Input from './Form/Input';
+import Radio from './Form/Radio';
 import Select from './Form/Select';
 
 const App = () => {
@@ -8,10 +9,22 @@ const App = () => {
   const [email, setEmail] = React.useState('');
   // produto = selectHTML
   const [produto, setProduto] = React.useState('');
+  // [cor, fruta] = inputRadioHTML
+  const [cor, setCor] = React.useState('Azul');
+  const [fruta, setFruta] = React.useState('Azul');
 
   return (
     <>
       <form>
+        {/* passando os parametros para o campo de input Radio */}
+        <h2>Cores</h2>
+        <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+        <h2>Frutas</h2>
+        <Radio
+          options={['Melancia', 'Pera', 'Maçã']}
+          value={fruta}
+          setValue={setFruta}
+        />
         {/* passando os parametros para o campo de select */}
         <Select
           options={['notebook', 'smartphone', 'tablet']}
