72afbc43fb91c9e8c763434f1e39347c4c888323
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2020-11-21 15:05:07 -0300

    Forms select componente

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Input from './Form/Input';
+import Select from './Form/Select';
 
 const App = () => {
   //usar os useStates dos formularios, sempre dentro do form completo, nunca no componente
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
+  // produto = selectHTML
+  const [produto, setProduto] = React.useState('');
 
   return (
     <>
       <form>
+        {/* passando os parametros para o campo de select */}
+        <Select
+          options={['notebook', 'smartphone', 'tablet']}
+          value={produto}
+          setValue={setProduto}
+        />
         {/* passando os parametros para o campo de input */}
         <Input
           id="nome"
